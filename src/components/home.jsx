799487a5baa2738ae7b36db235404546a0876cac
Matt Hurst <matt.adam.hurst@gmail.com>
2020-04-29 17:01:41 +0100

    h2 fixed

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
                 <h2 className="about">About Me</h2>
                 <p>Hi, I'm Matt, a West London based web developer.</p>
                 <p>In a previous life I worked in sales, but once I tried coding I was hooked. I love solving puzzles, logic problems, and above all creating things!</p>
-                <p>I have learnt to code through online resources, including paid courses from Udemy, and by pestering my web developer friends into teaching me.</p>
+                <p>I have learnt to code through online resources, including paid courses from Udemy, and by pestering my web developer friends for hints and tips.</p>
                 <p>I'm looking for a job where I can deliver great products whilst continuing to learn and advance my skillset.</p>
             </div>
         </div>
