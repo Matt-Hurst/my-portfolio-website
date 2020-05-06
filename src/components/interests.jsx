c79f0ad69c62368f74b95e009ff54223a342218a
Matt Hurst <matt.adam.hurst@gmail.com>
2020-05-06 12:26:28 +0100

    selected image stays in focus

--- src/components/interests.jsx
+++ src/components/interests.jsx
@@ -9,26 +9,33 @@ import BookPic from "../img/books.jpg"
 import ChallPic from "../img/toughmudder.jpg"
 
 const Interests = () => {
-   
+        const [imageClicked, setImageClicked] = useState("none");
         const [message, setMessage] = useState(<p className="welcome p-interests">Welcome to my interests page. Simply click on any image below to find out more.</p>);
    
+
     // handleClick function for each image, changing the message (setMessage) to <p>hobby info<p>
     const footballClicked = ()=> {
+        setImageClicked("football")
         setMessage(<p className="p-interests">I have been playing (low level) competitive football since I was five, a career that culminated last year with my 11-a-side team being crowned champions of London South's Seventh Division. Autographs available upon request..</p>)
     }
     const surfClicked = ()=> {
+        setImageClicked("surf")
         setMessage(<p className="p-interests">Whilst on a trip to Bali in 2012 I tried surfing for the first time and was hooked. Since then I have thrown myself at waves across the UK, Portugal, and Central America.</p>)
     }
     const travelClicked = ()=> {
+        setImageClicked("travel")
         setMessage(<p className="p-interests">I love travelling to new places and experiencing different cultures. My travels have taken me to Kyrgyzstan for the World Nomad Games, through Central America, and to South East Asia four times.</p>)
     }
     const bookClicked = ()=> {
-    setMessage(<p className="p-interests">There is no greater entertainment than a good book. I'm a big fan of historical fiction, sci-fi, and learning new things through non-fiction. My favourite author is James Clavell and I am always open to book recommendations.</p>)
+        setImageClicked("book")
+        setMessage(<p className="p-interests">There is no greater entertainment than a good book. I'm a big fan of historical fiction, sci-fi, and learning new things through non-fiction. My favourite author is James Clavell and I am always open to book recommendations.</p>)
     }
     const challengeClicked = ()=> {
+        setImageClicked("challenge")
         setMessage(<p className="p-interests">As someone who is always looking to push myself and improve, I like to have clear physical goals and a deadline to hit. I've completed three Tough Mudders and two half-marathons.</p>)
     }
     const foodClicked = ()=> {
+        setImageClicked("food")
         setMessage(<p className="p-interests">Certainly someone who lives to eat, I love eating good food - both going out to eat with friends and family and cooking at home with my girlfriend. Japanese cuisine is my favourite, especially Sushi and Ramen.</p>)
     }
 
@@ -43,7 +50,7 @@ const Interests = () => {
             <div className="interests">
                 <ul className="interests-showcase">
                     <li>
-                        <figure className="interest-pic"
+                        <figure className={ imageClicked === "football" ? "selected interest-pic" : "interest-pic" }
                         onClick={()=> {
                             footballClicked();
                          }}
@@ -52,7 +59,7 @@ const Interests = () => {
                         </figure>
                     </li>
                     <li>
-                        <figure className="interest-pic"
+                        <figure className={ imageClicked === "surf" ? "selected interest-pic" : "interest-pic" }
                         onClick={()=> {
                             surfClicked();
                          }}
@@ -61,7 +68,7 @@ const Interests = () => {
                         </figure>
                     </li>
                     <li>
-                        <figure className="interest-pic"
+                        <figure className={ imageClicked === "travel" ? "selected interest-pic" : "interest-pic" }
                         onClick={()=> {
                             travelClicked();
                          }}
@@ -70,7 +77,7 @@ const Interests = () => {
                         </figure>
                     </li>
                     <li>
-                        <figure className="interest-pic"
+                        <figure className={ imageClicked === "book" ? "selected interest-pic" : "interest-pic" }
                         onClick={()=> {
                             bookClicked();
                          }}
@@ -79,7 +86,7 @@ const Interests = () => {
                         </figure>
                     </li>
                     <li>
-                        <figure className="interest-pic"
+                        <figure className={ imageClicked === "challenge" ? "selected interest-pic" : "interest-pic" }
                         onClick={()=> {
                             challengeClicked();
                          }}
@@ -88,7 +95,7 @@ const Interests = () => {
                         </figure>
                     </li>
                     <li>
-                        <figure className="interest-pic"
+                        <figure className={ imageClicked === "food" ? "selected interest-pic" : "interest-pic" }
                         onClick={()=> {
                             foodClicked();
                          }}
